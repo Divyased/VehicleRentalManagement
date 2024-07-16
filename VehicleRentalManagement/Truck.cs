3615c8fa29086b3d81b29dbfe4e0320f51750fa2
Divya Sirling Edward <Dsirlingedward1882@conestogac.on.ca>
2024-07-15 23:21:59 -0400

    Truck.cs file has been created

---
+++ VehicleRentalManagement/Truck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Truck : Vehicle
+{
+    public double Capacity { get; set; }
+    public string TruckType { get; set; }
+    public bool FourWheelDrive { get; set; }
+    public Truck(string model, string manufacturer, int year, decimal rentalprice, double capacity,string trucktype, bool fourwheeldrive) : base(model,manufacturer,year,rentalprice) 
+    { 
+        Capacity = capacity;
+        TruckType = trucktype;
+        FourWheelDrive = fourwheeldrive;
+    }
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine("Capacity:" + " " +  Capacity);
+        Console.WriteLine("Truck Type:" + " " + TruckType);
+        Console.WriteLine("Four Wheel Drive:" + " " +  FourWheelDrive);
+    }
+}
+
