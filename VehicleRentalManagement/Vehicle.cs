d1416da9977b1ed298b274c11f82a1f7525d6ce4
Divya Sirling Edward <Dsirlingedward1882@conestogac.on.ca>
2024-07-15 20:00:59 -0400

    Add project files.

---
+++ VehicleRentalManagement/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Vehicle
+{
+    public string Model { get; set; }
+    public string Manufacturer { get; set; }
+    public int Year { get; set; }
+    public decimal RentalPrice { get; set; }
+
+    public Vehicle(string model, string manufacturer, int year, decimal rentalprice)
+    {
+        Model = model;
+        Manufacturer = manufacturer;
+        Year = year;
+        RentalPrice = rentalprice;
+    }
+    public virtual void DisplayDetails()
+    {
+        Console.WriteLine("Model:" + " " + Model);
+        Console.WriteLine("Manufacturer:" + " " + Manufacturer);
+        Console.WriteLine("Year:" + " " + Year);
+        Console.WriteLine("Rental price:" + " " + RentalPrice);
+
+    }
+}
