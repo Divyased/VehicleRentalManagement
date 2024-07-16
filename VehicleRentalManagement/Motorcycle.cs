bfdc882fcb9d464dfa4dad0ce9f29023879639ca
Divya Sirling Edward <Dsirlingedward1882@conestogac.on.ca>
2024-07-15 23:56:22 -0400

    Motorcycle.cs class has been created

---
+++ VehicleRentalManagement/Motorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Motorcycle : Vehicle
+{
+    public int EngineCapacity { get; set; }
+    public string FuelType { get; set; }
+    public bool HasFairing { get; set; }
+    public Motorcycle(string model, string manufacturer, int year, decimal rentalprice, int enginecapacity, string fueltype, bool hasfairing) : base(model,manufacturer,year,rentalprice) 
+    {
+        EngineCapacity = enginecapacity;
+        FuelType = fueltype;
+        HasFairing = hasfairing;
+    }
+    public override void DisplayDetails()
+    {
+        base.DisplayDetails();
+        Console.WriteLine("Engine Capacity:" + " " +  EngineCapacity + "cc");
+        Console.WriteLine("Fuel Type:" + " " +  FuelType);
+        Console.WriteLine("Has Fairing:" + " "+  HasFairing);
+    }
+}
