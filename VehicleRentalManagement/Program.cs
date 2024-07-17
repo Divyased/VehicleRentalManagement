fbb44a2f1489d9bc9c7e274e50416134e87fa177
Divya Sirling Edward <Dsirlingedward1882@conestogac.on.ca>
2024-07-17 15:53:36 -0400

    Updated program.cs file

--- VehicleRentalManagement/Program.cs
+++ VehicleRentalManagement/Program.cs
@@ -4,21 +4,24 @@ public class Program()
     public static void Main()
     {
         RentalAgency agency = new RentalAgency();
-        Car car1 = new Car("Chevrolet Camaro", "Chevrolet", 2023, 150, 4, "V8", "Manual", false);
-        Truck truck1 = new Truck("RAM 1500", "RAM", 2022, 180, 3.0, "Pickup", true);
-        Motorcycle motorcycle1= new Motorcycle("Street 750", "Harley-Davidson", 2020, 110, 750, "Petrol", true);
+        Car c1 = new Car("Chevrolet Camaro", "Chevrolet", 2023, 150, 4, "V8", "Manual", false);
+        Truck t1 = new Truck("RAM 1500", "RAM", 2022, 180, 3.0, "Pickup", true);
+        Motorcycle mc1= new Motorcycle("Street 750", "Harley-Davidson", 2020, 110, 750, "Petrol", true);
 
-        agency.AddVehicle(car1);
-        agency.AddVehicle(truck1);
-        agency.AddVehicle(motorcycle1);
+        agency.AddVehicle(c1);
+        agency.AddVehicle(t1);
+        agency.AddVehicle(mc1);
 
         Console.WriteLine("Fleet details are:");
+        Console.WriteLine("\n");
         agency.DisplayFleet();
 
         Console.WriteLine("\nRenting a vehicle:");
-        agency.RentVehicle(car1);
+        Console.WriteLine("\n");
+        agency.RentVehicle(c1);
 
         Console.WriteLine("\nAfter renting fleet details are:");
+        Console.WriteLine("\n");
         agency.DisplayFleet();
 
         Console.WriteLine("\nTotal Revenue:" + "" + agency.TotalRevenue.ToString("C"));
